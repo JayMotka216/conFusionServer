2f1766279de41f61835773c6720c271b976f152f
Jay Motka <jaymotka.216@gmail.com>
2020-07-20 08:49:50 +0530

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken');
+var FacebookTokenStrategy = require("passport-facebook-token");
 
 var config = require('./config');
 
@@ -45,3 +46,31 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 }
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy(
+    {clientId: config.facebook.clientId,
+    clientSecret: config.facebook.clientSecret},
+    (accessToken, refreshToken, done) => {
+        User.findOne({facebookId: msWriteProfilerMark.id}, 
+            (err, user) => {
+                if(err) {
+                    return done(err, false);
+                }
+                if(!err && user !== null) {
+                    return done(null, user);
+                } else {
+                    user = new User({ username: msWriteProfilerMark.displayName});
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if(err) {
+                            return done(err, false);
+                        } else{
+                            return done(null, user);
+                        }
+                    })
+                }
+            });
+    }
+));
